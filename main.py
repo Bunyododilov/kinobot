29b59b4baeabd0852dbb3e73e6229090c86ee968
Bunyododilov <156313999+Bunyododilov@users.noreply.github.com>
2025-07-12 15:02:31 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from aiogram import Bot, Dispatcher, types
-from aiogram.types import Message
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram import F
 import asyncio
 
-# ——— Replit/Render uchun server qismi
 from flask import Flask
 from threading import Thread
 
@@ -19,24 +18,42 @@ def run():
 def keep_alive():
     t = Thread(target=run)
     t.start()
-# ———————————————————
 
-# Bot tokeningiz
 bot = Bot(token="7874277238:AAFMjafESzwtn7eqGdreXvCLiiwUiUy_8Mk")
 dp = Dispatcher()
 
-# Kodlar va video file_id lar
 videos = {
     "34": "BAACAgQAAxkBAAMWaGirX43xZF8ei5hPR2HrI329EE8AAuwFAAKTMqBTkQNk7LUmAxY2BA"
 }
 
+required_channels = ["@TELEFON_BOZOR_24_UZB", "@AVTO_BOZOR_24_UZB"]
+
+async def is_subscribed(user_id):
+    for channel in required_channels:
+        try:
+            member = await bot.get_chat_member(chat_id=channel, user_id=user_id)
+            if member.status not in ["member", "creator", "administrator"]:
+                return False
+        except:
+            return False
+    return True
+
 @dp.message(F.text)
 async def send_video(message: Message):
-    code = message.text.strip()
-    if code in videos:
-        await message.answer_video(videos[code])
+    if await is_subscribed(message.from_user.id):
+        code = message.text.strip()
+        if code in videos:
+            await message.answer_video(videos[code])
+            await message.answer_sticker("CAACAgIAAxkBAAEI2oRk8WqU_m_owQABlfK6ucpIRasOmgACoAcAAhZCawABIS3ViWxZKzYE")
+        else:
+            await message.answer("❌ Bunday kodli video topilmadi.")
     else:
-        await message.answer("❌ Bunday kodli video topilmadi.")
+        keyboard = InlineKeyboardMarkup(inline_keyboard=[
+            [InlineKeyboardButton(text="📱 TELEFON_BOZOR_24_UZB", url="https://t.me/TELEFON_BOZOR_24_UZB")],
+            [InlineKeyboardButton(text="🚗 AVTO_BOZOR_24_UZB", url="https://t.me/AVTO_BOZOR_24_UZB")]
+        ])
+        await message.answer("❗ Kodni yuborishdan oldin quyidagi kanallarga to‘liq obuna bo‘ling. So‘ng qaytadan urinib ko‘ring:", reply_markup=keyboard)
+        await message.answer_sticker("CAACAgIAAxkBAAEI2oBk8Wp_sDRBG3U9D0ipkRIYQgOQzAACDgADVp29Cp77RBupC3mNGQQ")
 
 async def main():
     await dp.start_polling(bot)
@@ -44,3 +61,6 @@ async def main():
 if __name__ == "__main__":
     keep_alive()
     asyncio.run(main())
+
+
+ 
